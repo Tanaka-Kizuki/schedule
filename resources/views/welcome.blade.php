20e64ff78690f6e25f999fb3f874956b7004913d
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-04-20 17:41:58 +0900

    view welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,35 +65,22 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <table>
+                <tr>
+                    <th>会社名</th>
+                    <th>ホームページ</th>
+                    <th>募集要項</th>
+                    <th>状況</th>
+                </tr>
+                <tr>
+                    <th>株式会社クリーク・アンド・リバー社</th>
+                    <th><a href="https://www.cri.co.jp/">HP</a></th>
+                    <th>wantedlly</th>
+                    <th>書類落選</th>
+                </tr>
+                    
+                </table>
             </div>
         </div>
     </body>
