9fae1e20e32823ba7e2c9defe2c8b7303876c279
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-04-22 20:53:00 +0900

    debug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,16 +82,7 @@
                     <th>状況</th>
                     <th>面接日時</th>
                     <th>備考・媒体</th>
-                </tr>
-                <tr>
-                    @foreach($datas as $data)
-                    <th>{{$data->company}}</th>
-                    <th><a href="{{$data->url}}">HP</a></th>
-                    <th>{{$data->status}}</th>
-                    <th>{{$data->date}}</th>
-                    <th>{{$data->memos}}</th>
-                    @endforeach
-                </tr>                    
+                </tr>                   
                 </table>
             </div>
         </div>
