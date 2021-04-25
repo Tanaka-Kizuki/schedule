766199ba024e71319e3f98c1fab3f5757ebb5f90
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-04-25 23:24:25 +0900

    update view top

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -7,6 +7,13 @@
      <title>Document</title>
 </head>
 <body>
-     
+<h2 class="calendar-title"><span id="js-year"></span>年 <span id="js-month"></span>月</h2>
+<table class="calendar-table">
+  <thead>
+    <tr><th>日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th>土</th></tr>
+  </thead>
+  <tbody id="js-calendar-body"></tbody>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</table>
 </body>
 </html>
